0739094f43449cf1adf9d60f574abd7786e6434d
ReZero <ghostofpast@icloud.com>
2023-04-06 18:29:58 +0300

    complite tasks

---
+++ task001_PD/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Число: {number} - палиндром.");
+  }
+  else 
+  {
+    Console.WriteLine($"Число: {number} - не палиндром.");
+  }
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
---
+++ task002_Dot/Program.cs
@@ -0,0 +1,25 @@
+﻿//Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string axis, string point)
+{
+    Console.Write($"Введите координату {axis} точки {point}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2)
+                {
+                    return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+                }
+
+double linesegment =  Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {linesegment}");
